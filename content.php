b4c38c5006eabbac3b2cf67f76b4ad4ca0c261d9
bharathrangaraj <bharath.rangaraj@rwth-aachen.de>
2015-07-15 20:46:15 +0530

    more dynamic

--- content.php
+++ content.php
@@ -3,19 +3,7 @@ require_once("includes/functions.php");
 include("includes/header.php");
 ?>
 <?php
-if(isset($_GET["info"])){
-    $table1=get_info($_GET['info']);
-    $table2=NULL;
-}
-else if(isset($_GET["page"])){
-    $table2=get_page($_GET['page']);
-    $table1=NULL;
-}
-else{
-    $table1=NULL;
-    $table2=NULL;
-}
-
+find_selected_page();
 
 
 ?>
@@ -28,32 +16,9 @@ else{
         <table class="tables">
             <tr>
                 <td id="nav2">
-                    <ul class="menu">
-                        <?php
-
-                        $result=get_content_menu();
-                        while($row=mysql_fetch_array($result)){
-
-                            echo "<li> <a ";
-                            if($row['id']==$table1['id']){
-                                echo "class=\"selected\"";
-
-                            }
-
-                                    echo "href=\"content.php?info=".urlencode($row['id'])."\"> {$row['menu']}</a></li>";
-                            $result2=get_content_submenu($row[2]);
-                            while($row2=mysql_fetch_array($result2)){
-                                echo "<ul class=\"submenu\">";
-                                echo "<li><a ";
-                                if($row2['id']==$table2['id']){
-                                    echo "class=\"selected\"";
-                                }
-                                echo " href=\"content.php?page=".urlencode($row2['id'])."\">{$row2['menu']}</a></li>";
-                                echo "</ul>";
-                            }
-                        }
-                        ?>
-                    </ul>
+                    <?php
+                    get_navigation($table1,$table2);
+                    ?>
 
                     <br><br>
                     <div id="new">
--- includes/functions.php
+++ includes/functions.php
@@ -68,5 +68,45 @@ function get_page($page_id){
 }
 
 
+function find_selected_page(){
+    global $table1;
+    global $table2;
+    if(isset($_GET["info"])){
+        $table1=get_info($_GET['info']);
+        $table2=NULL;
+    }
+    else if(isset($_GET["page"])){
+        $table2=get_page($_GET['page']);
+        $table1=NULL;
+    }
+    else{
+        $table1=NULL;
+        $table2=NULL;
+    }
+}
+
+function get_navigation($table1,$table2){
+    echo "<ul class=\"menu\">";
+    $result=get_content_menu();
+    while($row=mysql_fetch_array($result)){
+        echo "<li> <a ";
+        if($row['id']==$table1['id']){
+            echo "class=\"selected\"";
+        }
+        echo "href=\"content.php?info=".urlencode($row['id'])."\"> {$row['menu']}</a></li>";
+        $result2=get_content_submenu($row[2]);
+        while($row2=mysql_fetch_array($result2)){
+            echo "<ul class=\"submenu\">";
+            echo "<li><a ";
+            if($row2['id']==$table2['id']){
+                echo "class=\"selected\"";
+            }
+            echo " href=\"content.php?page=".urlencode($row2['id'])."\">{$row2['menu']}</a></li>";
+            echo "</ul>";
+        }
+    }
+    echo "</ul>";
+}
+
 ?>
 
